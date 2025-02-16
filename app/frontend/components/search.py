ceab25170e1efb52b2014646c32d3a5e8052a074
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-02-15 22:42:23 -0300

    feat: reconver funcs and new theme button

--- app/frontend/components/search.py
+++ app/frontend/components/search.py
@@ -7,6 +7,7 @@ class Search(ft.Container):
             download_function,
             placeholder: str = "Search for videos",
             icon: ft.Icon = ft.Icons.SEARCH,
+            output_placeholder: str = "File Name",
             **kwargs,
         ):
         super().__init__(**kwargs)
@@ -14,26 +15,53 @@ class Search(ft.Container):
             hint_text=placeholder,
             width=600,
         )
+        self.output = ft.TextField(
+            hint_text=output_placeholder,
+            width=600,
+        )
         self.button = ft.ElevatedButton(
             icon=icon,
             text="Search",
             bgcolor=ft.Colors.TRANSPARENT, 
+            color=ft.Colors.GREY_800,
             on_click=self.download,
             width=100,
             height=50
         )
-        self.content = ft.Row([self.input, self.button])
-        self.content.alignment = ft.MainAxisAlignment.CENTER
+        self.content = ft.Column(
+            [
+                ft.Row(
+                    [
+                        ft.Container(
+                            self.input,
+                        ), 
+                        ft.Container(
+                            self.button,
+                        ) 
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Container(
+                    self.output,
+                    margin=ft.Margin(225, 0, 0, 0)
+                )
+                
+            ]
+            
+            
+        )
         #self.padding = 100
         self.margin = (50)
         self.download_function = download_function
 
     def download(self, e):
         value = self.input.value
-        self.download_function(value, "videos", f"teste-search-{datetime.now()}")
+        file_name = self.output.value if self.output.value else f"video-{datetime.now()}"
+        self.download_function(value, "videos", file_name)
 
     def clean_field(self):
         self.input.value = None
-        print("Limpei")
-        print(self.input.value)
+        self.output.value = None
         self.update()
+        
+# https://flet.dev/docs/controls/container
--- app/frontend/main.py
+++ app/frontend/main.py
@@ -13,6 +13,36 @@ def main(page: ft.Page):
     page.title = "Downloader Youtube Videos"
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    
+    page.window.width = 800
+    page.window.height = 600
+    page.window.max_width = 800
+    page.window.max_height = 600
+    page.window.full_screen = False
+    page.window.resizable = False
+    page.window.movable = True
+
+    page.bgcolor = ft.Colors.WHITE
+    
+    is_dark_mode = False
+    
+    def change_theme(event):
+        nonlocal is_dark_mode
+        is_dark_mode = not is_dark_mode
+        page.bgcolor = ft.Colors.GREY_900 if is_dark_mode else ft.Colors.WHITE
+        search.input.bgcolor = ft.Colors.GREY_700 if is_dark_mode else ft.Colors.GREY_300
+        search.input.color = ft.Colors.WHITE if is_dark_mode else ft.Colors.BLACK
+        search.output.bgcolor = ft.Colors.GREY_700 if is_dark_mode else ft.Colors.GREY_300
+        search.output.color = ft.Colors.WHITE if is_dark_mode else ft.Colors.BLACK
+        search.button.bgcolor = ft.Colors.GREY_700 if is_dark_mode else ft.Colors.GREY_300
+        search.button.color = ft.Colors.WHITE if is_dark_mode else ft.Colors.BLACK
+        change_theme_button.icon = ft.Icons.DARK_MODE if is_dark_mode else ft.Icons.LIGHT_MODE
+        page.update()
+    
+    change_theme_button = ft.IconButton(
+        ft.Icons.LIGHT_MODE,
+        on_click=change_theme
+    )
 
 
     def handle_download(url: str, output: str, file_name: str) -> None:
@@ -33,13 +63,17 @@ def main(page: ft.Page):
     search = Search(handle_download)
 
     alert_dialog = AlertDialog(
-        title="Seu download foi concluído com sucesso!",
+        title="Download successful!",
         content=None,
         on_dismiss=lambda _: search.clean_field(),
     )
     
 
     page.add(
+        ft.Container(
+            change_theme_button,
+            alignment=ft.alignment.top_right,
+        ),
         search,
         ft.Container(
             content=loading_indicator,
@@ -47,8 +81,10 @@ def main(page: ft.Page):
             expand=True,  # Ocupa toda a tela para centralizar o indicador
             bgcolor="transparent",  # Fundo semitransparente (opcional)
         ),
-        alert_dialog
+        alert_dialog,
     )
+    
+    change_theme(None)
 
    
     
